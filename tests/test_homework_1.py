bdd1ece822910790c5b4454b2a304869e159e8c6
gerfx00 <gerfx00@gmail.com>
2024-10-01 18:45:40 +0300

    delete XFAIL

--- tests/test_homework_1.py
+++ tests/test_homework_1.py
@@ -7,7 +7,6 @@ from async_asgi_testclient import TestClient
 from lecture_1.hw.math_plain_asgi import app
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("method", "path"),
@@ -27,7 +26,6 @@ async def test_not_found(method: str, path: str):
         assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("query", "status_code"),
@@ -51,7 +49,6 @@ async def test_factorial(query: dict[str, Any], status_code: int):
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     ("params", "status_code"),
@@ -72,7 +69,6 @@ async def test_fibonacci(params: str, status_code: int):
         assert "result" in response.json()
 
 
-@pytest.mark.xfail()
 @pytest.mark.asyncio()
 @pytest.mark.parametrize(
     ("json", "status_code"),
