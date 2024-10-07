99d86e7a22cb15e3c622331bb7959f00a38e4b08
Artur Musaev <cormeum@MacBook-Air-Artur.local>
2024-10-08 01:18:09 +0300

    hw 3

--- lecture_2/hw/main.py
+++ lecture_2/hw/main.py
@@ -7,8 +7,11 @@ from fastapi import FastAPI
 from shop_api.database.database import Base, engine
 from shop_api.routes import routes
 
+from prometheus_fastapi_instrumentator import Instrumentator
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(routes.router)
