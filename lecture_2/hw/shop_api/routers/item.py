0e2d09756c0a81c01cb813b7150895c43f30bd3b
dimaioksha <enakentiy.lorens@yandex.ru>
2024-10-09 13:56:11 +0300

    Feat: Fix tests x2

--- lecture_2/hw/shop_api/routers/item.py
+++ lecture_2/hw/shop_api/routers/item.py
@@ -26,15 +26,15 @@ async def get_item(item_id: int):
 
 @router.get("/")
 async def get_items(offset: int = Query(0, ge=0),
-                   limit: int = Query(10, gt=0),
-                   min_price: Optional[float] = Query(None, gt=0),
-                   max_price: Optional[float] = Query(None, gt=0),
-                   show_deleted: Optional[bool] = False
-                   ):
+                    limit: int = Query(10, gt=0),
+                    min_price: Optional[float] = Query(None, gt=0),
+                    max_price: Optional[float] = Query(None, gt=0),
+                    show_deleted: Optional[bool] = False
+                    ):
     filtered_items: List[Item] = list(filter(lambda item: (
-        (min_price is None or item.price >= min_price) and
-        (max_price is None or item.price <= max_price) and
-        (show_deleted or not item.deleted)
+            (min_price is None or item.price >= min_price) and
+            (max_price is None or item.price <= max_price) and
+            (show_deleted or not item.deleted)
     ), items_db.values()))
     filtered_items = filtered_items[offset:offset + limit]
     return filtered_items
@@ -46,12 +46,15 @@ async def put_item(item_id: int, item: ItemPut):
     return items_db[item_id]
 
 
-
 @router.patch("/{item_id}")
-async def patch_item(item_id: int, item: ItemUpdate):
+async def patch_item(item_id: int, new_item: Optional[ItemUpdate] = None):
     item = items_db.get(item_id)
     if item and not item.deleted:
-        items_db[item_id] = Item(id=item_id, name=item.name, price=item.price, deleted=items_db[item_id].deleted)
+        if new_item is None:
+            return items_db[item_id]
+        new_name = new_item.name if new_item.name is not None else item.name
+        new_price = new_item.price if new_item.price is not None else item.price
+        items_db[item_id] = Item(id=item_id, name=new_name, price=new_price, deleted=item.deleted)
         return items_db[item_id]
     else:
         raise HTTPException(status_code=http.HTTPStatus.NOT_MODIFIED, detail="Item not modified")
@@ -62,7 +65,5 @@ async def delete_item(item_id: int):
     item = items_db.get(item_id)
     if not item:
         raise HTTPException(status_code=http.HTTPStatus.NOT_FOUND, detail="Item not found")
-    items_db[item_id] = Item(id=item_id, name=items_db[item_id].name, price=items_db[item_id].price, deleted=True)
+    items_db[item_id] = Item(id=item_id, name=item.name, price=item.price, deleted=True)
     return items_db[item_id]
-
-
