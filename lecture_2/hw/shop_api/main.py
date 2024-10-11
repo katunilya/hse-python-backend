0b34f7305be0c0c6d0efb9d5e6e577ad5705bb75
DanilMargaryan <margaryan.danil@mail.ru>
2024-10-11 21:42:25 +0300

    add hw3

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from .routers import items, carts
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(items.router)
 app.include_router(carts.router)
